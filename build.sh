27f652ff45e0cccb5967dbec133553a0859ccfe4
dmfedorin <91863731+dmfedorin@users.noreply.github.com>
2022-01-20 14:16:05 +0400

    Delete build.sh

--- build.sh
+++
@@ -1,7 +0,0 @@
-nasm -o build/imed/boot.bin -f bin src/boot/boot.asm
-
-#i386-elf-gcc -T linker.ld -o build/final/glucOS.bin build/imed/*.o -ffreestanding -nostdlib -lgcc
-
-cat build/imed/boot.bin > build/final/glucOS.bin
-
-qemu-system-x86_64 --drive format=raw,file=build/final/glucOS.bin
