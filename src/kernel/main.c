2e34b48fef3335c685be8d65d5faad268b23ed98
tirimid <q44q44q44q44@gmail.com>
2022-02-02 22:16:11 +0400

    forgot something in last commit

--- src/kernel/main.c
+++ src/kernel/main.c
@@ -7,8 +7,8 @@ void krnl_main(void)
         term_print("[glucOS]\r\n\n");
         
         struct mem_layout_entry entries[16];
-        mem_get_layout(entries, 16);
-        
+        mem_get_layout_entries(entries, 16);
+
         term_print_hex(mem_get_memory_bytes(entries, MEM_LAYOUT_ENTRY_TYPE_FREE));
         term_print("\r\n\n");
 
